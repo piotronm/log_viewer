c8302d34aaaf7e9c39be6d061dffc4e38cf44dc6
piotronm <123025664+piotronm@users.noreply.github.com>
2023-04-21 15:31:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
 import "./App.css";
 import Header from './Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
+import { BrowserRouter as Router} from 'react-router-dom';
+import moment from 'moment-timezone';
 
 function App() {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [logType, setLogType] = useState('');
   const [fileSize, setFileSize] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [selectedContentId, setSelectedContentId] = useState('');
+  const [selectedModuleName, setSelectedModuleName] = useState('');
+  const [sortOrder, setSortOrder] = useState("newest");
 
-
-  function getClass(logType) {
-    switch (logType) {
-      case 'info':
-        return 'info';
-      case 'warn':
-        return 'warn';
-      case 'error':
-        return 'error';
-      default:
-        return '';
+  function getClass(str) {
+    if (str && str.length >= 31) {
+      const code = str.slice(27, 31);
+      if (code === "0001") {
+        return "green";
+      } else if (code === "0002") {
+        return "red";
+      } else if (code === "0003") {
+        return "blue";
+      }
     }
+    return "";
   }
   
-
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -39,14 +41,142 @@ function App() {
   
     reader.onload = () => {
       const lines = reader.result.split('\n');
-      setData(lines);
+      const convertedData = lines.map(line => {
+        const date = moment.utc(line.slice(0, 23)).tz(moment.tz.guess());
+        const restOfLine = line.slice(24);
+        const convertedDate = date.tz(moment.tz.guess()).format('YYYY-MM-DD h:mm:ss A');
+        return `${convertedDate} ${restOfLine}`;
+      });
+      setData(convertedData);
+
+  // Extract module names from file and populate dropdown
+  const moduleNames = new Set();
+  const moduleNameRegex = /\[([^\[\]]*?)\]/gi;
+  lines.forEach((line) => {
+    const matches = line.match(moduleNameRegex);
+    if (matches) {
+      matches.forEach((match) => {
+        const moduleName = match.slice(1, -1); // Remove brackets
+        if (moduleName.indexOf('contentid') === -1) { // Check if moduleName doesn't contain "contentid"
+          moduleNames.add(moduleName);
+        }
+      });
+    }
+  });
+  
+  // Add "All" option to module name dropdown and set it as the default value
+moduleNames.add('All');
+const moduleNameDropdown = document.getElementById('module-name-dropdown');
+if (moduleNameDropdown) {
+  moduleNameDropdown.innerHTML = '';
+  const optionAll = document.createElement('option');
+  optionAll.value = 'All';
+  optionAll.text = 'All';
+  moduleNameDropdown.appendChild(optionAll);
+  moduleNames.forEach((moduleName) => {
+    // Check if moduleName contains only letters and numbers
+    const regex = /^[a-zA-Z0-9]*$/;
+    if (regex.test(moduleName) && moduleName !== 'All') {
+      const option = document.createElement('option');
+      option.value = moduleName;
+      option.text = moduleName;
+      moduleNameDropdown.appendChild(option);
+    }
+  });
+  moduleNameDropdown.value = 'All';
+}
+
+      // Extract content IDs from file and populate dropdown
+      const contentIds = new Set();
+      const contentIdRegex = /contentid-([0-9a-z]+)/gi;
+      lines.forEach((line) => {
+        const matches = line.match(contentIdRegex);
+        if (matches) {
+          matches.forEach((match) => {
+            const contentId = match.replace('contentid-', '');
+            contentIds.add(contentId);
+          });
+        }
+      });
+      // Add "All" option to content ID dropdown and set it as the default value
+      contentIds.add('All');
+      const contentIdDropdown = document.getElementById('contentid-dropdown');
+      if (contentIdDropdown) {
+        contentIdDropdown.innerHTML = '';
+          const optionAll = document.createElement('option');
+          optionAll.value = 'All';
+          optionAll.text = 'All';
+          contentIdDropdown.appendChild(optionAll);
+          contentIds.forEach((contentId) => {
+      if (contentId !== 'All') {
+        const option = document.createElement('option');
+        option.value = contentId;
+        option.text = contentId;
+        contentIdDropdown.appendChild(option);
+        }
+      });
+    contentIdDropdown.value = 'All';
+      }
     };
     reader.readAsText(file);
-  
     const fileSizeInMB = file.size / (1024 * 1024);
     setFileSize(fileSizeInMB.toFixed(3));
   };
-  
+
+  const handleExport = () => {
+    const filterInfo = `Filters:
+    Search term: ${searchTerm || 'none'}
+    Log type: ${logType || 'none'}
+    File size: ${fileSize || 'none'} MB
+    Start date: ${startDate || 'none'}
+    End date: ${endDate || 'none'}
+    Content ID: ${selectedContentId || 'none'}
+    Module name: ${selectedModuleName || 'none'}
+    Sort order: ${sortOrder}`;
+    const filteredDataText = `${filterInfo}\n\n${data.filter((item) => {
+      // Filter based on selectedModuleName
+      if (selectedModuleName !== 'All' && !item.includes(`[${selectedModuleName}]`)) {
+        return false;
+      }
+      // Filter based on searchTerm
+      if (searchTerm && !item.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return false;
+      }
+      // Filter based on logType
+      if (logType && !item.toLowerCase().includes(logType.toLowerCase())) {
+        return false;
+      }
+      // Filter based on selectedContentId
+      if (selectedContentId && selectedContentId !== 'All' && !item.includes(selectedContentId)) {
+        return false;
+      }
+      // Filter based on startDate
+      if (startDate && moment.utc(item.slice(0, 23)).isBefore(moment.utc(startDate))) {
+        return false;
+      }
+      // Filter based on endDate
+      if (endDate && moment.utc(item.slice(0, 23)).isAfter(moment.utc(endDate))) {
+        return false;
+      }
+      return true;
+    }).join('\n')}`;
+    
+    
+    const blob = new Blob([filteredDataText], {type: "text/plain;charset=utf-8"});
+    const fileName = "filtered_data.txt";
+    
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      // For IE browser
+      window.navigator.msSaveOrOpenBlob(blob, fileName);
+    } else {
+      // For other browsers
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      link.click();
+    }
+  };
 
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
@@ -54,81 +184,162 @@ function App() {
 
   const handleLogTypeChange = (event) => {
     setLogType(event.target.value);
-  }
+  };
+
+  const handleContentIdChange = (event) => {
+    const value = event.target.value;
+    setSelectedContentId(value === 'All' ? '' : value);
+  };  
+
+  const handleModuleNameChange = (event) => {
+    const value = event.target.value;
+    setSelectedModuleName(value === 'All' ? '' : value);
+  };  
+  
+  const handleStartDateChange = (event) => {
+    const selectedDate = event.target.value;
+    const startDate = selectedDate ? new Date(selectedDate) : null;
+    setStartDate(startDate);
+  };
+  
+  const handleEndDateChange = (event) => {
+    const selectedDate = event.target.value;
+    const endDate = selectedDate ? new Date(selectedDate) : null;
+    setEndDate(endDate);
+  };
+  
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === "newest" ? "oldest" : "newest");
+  };
+  
+  const options = {
+    timeZone: "America/New_York",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
   const filteredData = data
-    .filter((item) => {
-      if (logType === '') {
-        return true;
-      } else {
-        return item.toLowerCase().includes(logType.toLowerCase());
-      }
-    })
-    .filter((item) => {
-      if (searchTerm === '') {
-        return true;
-      } else {
-        return item.toLowerCase().includes(searchTerm.toLowerCase());
-      }
-    });
+  .filter((item) => {
+    // Filter based on searchTerm
+    if (searchTerm && !item.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return false;
+    }
+    // Filter based on logType
+    if (logType && !item.toLowerCase().includes(logType.toLowerCase())) {
+      return false;
+    }
+    // Filter based on selectedContentId
+    if (selectedContentId && !item.includes(`contentid-${selectedContentId}`)) {
+      return false;
+    }
+    // Filter based on selectedModuleName
+    if (selectedModuleName && !item.includes(`[${selectedModuleName}]`)) {
+      return false;
+    }
+    // Filter based on startDate
+    if (startDate && new Date(item.split(" ")[0]) < startDate) {
+      return false;
+    }
+    // Filter based on endDate
+    if (endDate && new Date(item.split(" ")[0]) > endDate) {
+      return false;
+    }
+    return true;
+  })
+  .sort((a, b) => {
+    // Sort based on sortOrder
+    const dateA = new Date(a.split(" ")[0] + " " + a.split(" ")[1] + " " + options.timeZone);
+    const dateB = new Date(b.split(" ")[0] + " " + b.split(" ")[1] + " " + options.timeZone);
+    if (sortOrder === "newest") {
+      return dateB - dateA;
+    } else {
+      return dateA - dateB;
+    }
+  });
 
-    return (
-      <Router>
+if (sortOrder === "oldest") {
+  filteredData.reverse();
+}
+
+  return (
+    <Router>
+      <div id='top'>
+        <div className='header'>
+          <Header/>
+        </div>
         <div>
-          <div className='header'>
-            <Header/>
-            <Routes>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-            </Routes>
-          </div>
-          <br/> <br/> <br/> <br/>
-          <h1>File Upload and Sorting</h1>
-          <div>
-            <input type="file" onChange={handleFileUpload} /> 
-          </div>
-          {fileSize && (
-          <div>
-            File size: {fileSize} MB
-          </div>
-          )}
-          <div>
-            <br/>
-            <label htmlFor="search-input">Search:</label>
-            <input type="text" id="search-input" value={searchTerm} onChange={handleSearchInput} />
-          </div>
-          <div>
-            <label htmlFor="log-types">Log Types:</label>
-            <select id="log-types" value={logType} onChange={handleLogTypeChange}>
-              <option value="">All</option>
-              <option value="Info">Info</option>
-              <option value="Warn">Warn</option>
-              <option value="Error">Error</option>
+          <label for="file-upload" class="custom-file-upload">Choose File</label>
+          <input type="file" id='file-upload' onChange={handleFileUpload} /> 
+        </div>
+        <div className='file-size'>
+        <div>
+          File size: {fileSize} MB
+        </div>
+        </div>
+        <div>
+          <br/>
+          <label htmlFor="search-input" id='search-input'className='search-input'>Search:</label>
+          <input type="text" id="search-input" value={searchTerm} onChange={handleSearchInput} />
+        </div>
+        <div>
+          <button onClick={toggleSortOrder}>Currently Displaying {sortOrder === "newest" ? "Oldest - Newest" : "Newest - Oldest"}</button>
+        </div>
+        <div class='form-group'>
+          <label htmlFor="log-types">Log Level:</label>
+          <select id="log-types" value={logType} onChange={handleLogTypeChange}>
+            <option value="">All</option>
+            <option value="- Info:">Info</option>
+            <option value="- Warn:">Warn</option>
+            <option value="- Error:">Error</option>
+          </select>
+          <label htmlFor="contentid-dropdown">Content ID:</label>
+          <select id="contentid-dropdown" value={selectedContentId} onChange={handleContentIdChange}>
+          {/* options */}
+          </select>
+          <label for="module-name">Module Name:</label>
+          <select id="module-name-dropdown" value={selectedModuleName} onChange={handleModuleNameChange}>
+            {/* options */}
             </select>
-          </div>
-          <ul>
-            {filteredData.map((item, index) => {
-              const parts = item.split(',');
-              const display = searchTerm ? (
-                <span>
-                  {parts[0].replace(new RegExp(`(${searchTerm})`, 'gi'), '$1')} - {parts[1]}
-                </span>
-              ) : (
-                <span>{item}</span>
-              );
-              return (
-                <li key={index} className={getClass(parts[2])}>
-                  {display}
-                </li>
-              );
+        </div>
+        <div>
+          <label>Start Date and Time:</label>
+            <input type="datetime-local" onChange={handleStartDateChange} />
+          <label>End Date and Time:</label>
+            <input type="datetime-local" onChange={handleEndDateChange} />
+        </div>
+        <div>
+          <button onClick={handleExport}>Export</button>
+        </div>
+        <ul className='display-items'>
+          {filteredData.map((item, index) => {
+            const parts = item.split(',');
+            const regex = new RegExp(`(${searchTerm})`, 'gi');
+            const partsHighlighted = parts[0].split(regex);
+            const display = searchTerm ? (
+          <span>
+            {partsHighlighted.map((part, i) =>
+            regex.test(part) ? <mark key={i} className="highlight">{part}</mark> : part
+            )}
+            - {parts[1]}
+          </span>
+          ) : (
+          <span>{item}</span>
+          );
+          return (
+            <li key={index} className={getClass(parts[2])}>
+              {display}
+            </li>
+          );
             })}
           </ul>
-          <div className='footer'>
-          </div>
+        <div id='bottom'>
         </div>
-      </Router>
-    );
-}    
-
+      </div>
+    </Router>
+  );    
+}
 export default App
