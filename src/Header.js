2ac9adb21ddb6c17b3871c06016ec4e383bb4c5a
piotronm <123025664+piotronm@users.noreply.github.com>
2023-04-21 12:22:01 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,29 +7,34 @@ function Header() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const handleAboutClick = () => {
+    const top = document.getElementById('top');
+    top.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleContactClick = () => {
+    const bottom = document.getElementById('bottom');
+    bottom.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <nav className='header'>
       <Link to='/' className='header-logo' onClick={closeMobileMenu}>
-        Log Viewer
+        <h1>Log Viewer</h1>
       </Link>
       <div className='header-menu-icon' onClick={handleClick}>
         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
       </div>
       <ul className={click ? 'header-menu active' : 'header-menu'}>
         <li className='header-item'>
-          <Link to='/' className='header-link' onClick={closeMobileMenu}>
-            Home
-          </Link>
+          <button className='header-link' onClick={handleAboutClick}>
+            Top of Page
+          </button>
         </li>
         <li className='header-item'>
-          <Link to='/about' className='header-link' onClick={closeMobileMenu}>
-            About
-          </Link>
-        </li>
-        <li className='header-item'>
-          <Link to='/contact' className='header-link' onClick={closeMobileMenu}>
-            Contact
-          </Link>
+          <button className='header-link' onClick={handleContactClick}>
+            Bottom of Page
+          </button>
         </li>
       </ul>
     </nav>
